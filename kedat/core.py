255754c2073effe439df2c8aa47c140fde955da6
hkmshb <hkmshb@gmail.com>
2015-10-24 12:18:38 +0100

    Added XlSheet class.

--- kedat/core.py
+++ kedat/core.py
@@ -1,6 +1,8 @@
 """
 Defines the core functions and classes for kedat.
 """
+import os
+from openpyxl import load_workbook, Workbook
 
 
 
@@ -27,4 +29,96 @@ class Storage(dict):
     
     def __repr__(self):
         return "<Storage %s>" % dict.__repr__(self)
+
+
+class XlSheet:
+    """Represents a light wrapper around openpyxl's Worksheet object. Provides
+    convenient ways of iterating over rows which are presented as tuples.
+    """
+    
+    def __init__(self, source, sheet_name, row_offset=0, col_offset=0):
+        wb = source if type(source) is Workbook else None
+        if not wb and type(source) is str:
+            if not os.path.isfile(source):
+                raise FileNotFoundError(source)
+            
+            wb = load_workbook(source, read_only=False)
+        
+        if not wb:
+            raise ValueError(
+                "Expected types for source: str, Workbook. Type provided: %s" %
+                (type(source),)
+            )
+        
+        wb_sheet_names = wb.get_sheet_names()
+        if not sheet_name in wb_sheet_names:
+            raise ValueError(
+                "Sheet by the name '%s' not found in '%s'." %
+                (sheet_name, wb_sheet_names)
+            )
+        
+        self.worksheet = wb[sheet_name]
+        self.sheet_name = sheet_name
+        self.__col_offset = col_offset
+        self.__row_offset = row_offset
+        self.__generator = None
+        self.__current = None
+    
+    @property
+    def current(self):
+        return self.__current
+    
+    @property
+    def max_column(self):
+        return self.worksheet.max_column
+    
+    @property
+    def max_row(self):
+        return self.worksheet.max_row
+    
+    @property
+    def col_offset(self):
+        return self.__col_offset
+    
+    @col_offset.setter
+    def col_offset(self, value):
+        self.__col_offset = value
+        self.__generator = None
+    
+    @property
+    def row_offset(self):
+        return self.__row_offset
+    
+    @row_offset.setter
+    def row_offset(self, value):
+        self.__row_offset = value
+        self.__generator = None
+    
+    def iter_rows(self, row_offset=0):
+        self.row_offset = row_offset
+        return self.__get_generator()
+    
+    def next(self):
+        return self.__get_generator().send(None)
+    
+    def reset(self):
+        self.__generator = None
+    
+    def __iter__(self):
+        return self.__get_generator()
+    
+    def __get_generator(self):
+        def make_generator():
+            for i in range(self.row_offset + 1, self.max_row + 1):
+                row = []
+                for j in range(self.col_offset + 1, self.max_column + 1):
+                    value = self.worksheet.cell(row=i, column=j).value
+                    row.append(value)
+                
+                self.__current = row = tuple(row)
+                yield row
+                
+        if self.__generator is None:
+            self.__generator = make_generator()
+        return self.__generator
 
--- kedat/tests.py
+++ kedat/tests.py
@@ -1,9 +1,12 @@
 """
 Defines unittests for kedat.core module.
 """
+import os
 import unittest
 from unittest import TestCase
-from .core import Storage
+
+from .core import Storage, XlSheet
+from openpyxl.reader.excel import load_workbook
 
 
 
@@ -30,6 +33,92 @@ class StorageTestCase(TestCase):
     def test_access_using_unknown_key_returns_None(self):
         foo = Storage()
         self.assertIsNone(foo['bar'])
+
+
+class XlSheetTestCase(TestCase):
+    base_dir = os.path.dirname(__file__)
+    
+    def _get_xlsheet(self, row_offset=0):
+        filepath = os.path.join(self.base_dir, 'fixtures', 'school.xlsx')
+        xsht = XlSheet(filepath, 'students', row_offset=row_offset)
+        return xsht
+    
+    def test_can_be_created_using_workbook_filepath(self):
+        xsht = self._get_xlsheet()
+        self.assertIsNotNone(xsht)
+    
+    def test_cannot_create_from_non_existing_file(self):
+        filepath = '/tmp/schools.xlsx'
+        with self.assertRaises(FileNotFoundError):
+            XlSheet(filepath, 'students')
+    
+    def test_can_be_created_using_workbook_object(self):
+        filepath = os.path.join(self.base_dir, 'fixtures', 'school.xlsx')
+        wb = load_workbook(filepath)
+        xsht = XlSheet(wb, 'students')
+        self.assertIsNotNone(xsht)
+    
+    def test_cannot_create_from_non_Workbook_object(self):
+        wb = object()
+        with self.assertRaises(ValueError):
+            XlSheet(wb, 'students')
+    
+    def test_can_iterate_rows_using_object(self):
+        idx = 0
+        for row in self._get_xlsheet():
+            idx += 1
+            self.assertIsNotNone(row)
+        self.assertEqual(12, idx)
+    
+    def test_can_iterate_rows_using_next(self):
+        xsht = self._get_xlsheet()
+        idx, row = 0, xsht.next()
+        try:
+            while (row != None):
+                idx += 1
+                row = xsht.next()
+        except:  # catches StopIteration exception throw at end of iteration
+            pass
+        self.assertEqual(12, idx)
+    
+    def test_can_iterate_rows_beginning_at_an_offset(self):
+        idx = 1
+        for row in self._get_xlsheet(row_offset=5):
+            idx += 1
+        self.assertEqual(8, idx)
+    
+    def test_can_combine_next_and_object_iteration_for_enumeration(self):
+        xsht = self._get_xlsheet()
+        for i in range(5):
+            row = xsht.next()
+        
+        for r in xsht:
+            i += 1
+        self.assertEqual(11, i)
+    
+    def test_can_reset_iteration_halfway_using_row_offset_attribute(self):
+        xsht = self._get_xlsheet()
+        for i in range(7):
+            row = xsht.next()
+        self.assertEqual('sn', row[0])
+        
+        xsht.row_offset = 0
+        for i in range(9):
+            row = xsht.next()
+        self.assertEqual(2, row[0])
+        self.assertEqual('Jane Doe', row[1]) 
+    
+    def test_can_reset_iteration_calling_reset(self):
+        xsht = self._get_xlsheet()
+        for i in range(7):
+            row = xsht.next()
+        self.assertEqual('sn', row[0])
+        
+        xsht.reset()
+        for i in range(9):
+            row = xsht.next()
+        self.assertEqual(2, row[0])
+        self.assertEqual('Jane Doe', row[1])
 
 
 
