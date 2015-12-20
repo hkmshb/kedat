87b9027ffff741d9928e4cb39538e35299350246
hkmshb <hkmshb@gmail.com>
2015-12-20 11:29:15 +0100

    Added function for reading distribution s/s; added location column for stations

--- scripts/network.py
+++ scripts/network.py
@@ -2,7 +2,7 @@
 Ad-hoc kedat script for working with power network data.
 """
 import os
-from pymongo import MongoClient
+import pymongo
 from openpyxl import load_workbook
 
 from kedat.core import XlSheet, Storage as _
@@ -40,30 +40,46 @@ class Reader:
             assets_gen = self._get_transmission_assets
         elif self.source_type == INJECTION:
             assets_gen = self._get_injection_assets
+        elif self.source_type == DISTRIBUTION:
+            assets_gen = self._get_distribution_assets 
         
         cols = self._get_cols_index_info()
         return assets_gen(cols)
     
+    def _ensure_headers_exist(self, sample_headers, row_offset=0):
+        """Returns the index at which provided headers are found, otherwise
+        an exception is raised.
+        """
+        index = XlSheet.find_headers(self.ws, sample_headers, row_offset)
+        if index == -1:
+            raise Exception("Headers not found: %s" % sample_headers)
+        return index
+    
+    def _get_row_entry(self, row, label, default=''):
+        return row[label] or default
+    
     def _get_cols_index_info(self):
         names, indexes = (), ()
         if self.source_type == TRANSMISSION:
-            names = ('ts_code', 'ts_name', 'xfmr_id', 'xfmr_cap', 
+            names = ('ts_code', 'ts_name', 'ts_loc', 'xfmr_id', 'xfmr_cap', 
                      'fdr_volt', 'fdr_name', 'fdr_code')
-            indexes = list(range(1, 5)) + list(range(8, 11))
-        elif self.source_type == INJECTION:
-            names = ('source', 'is_name', 'is_type', 'xfmr_id', 'xfmr_cap',
-                     'fdr_code', 'fdr_volt', 'fdr_name')
             indexes = list(range(1, 6)) + list(range(9, 12))
+        elif self.source_type == INJECTION:
+            names = ('source', 'is_name', 'is_loc', 'is_type', 'xfmr_id', 'xfmr_cap',
+                     'fdr_code', 'fdr_volt', 'fdr_name')
+            indexes = list(range(1, 7)) + list(range(10, 13))
+        elif self.source_type == DISTRIBUTION:
+            names = ('fdr_volt', 'fdr_name', 'ss_name', 'ss_cap', 'ss_type')
+            indexes = list(range(1, 3)) + list(range(4, 7))
         return _(zip(names, indexes))
     
     def _get_transmission_assets(self, cols):
         # helper
-        get = lambda r, label: r[label] or ''
+        get = self._get_row_entry
         
         # find headers
-        hdrs = ['sn', 'ts.code', 'ts.name', 'xfmr.id']
-        if not XlSheet.find_headers(self.ws, hdrs, 0):
-            raise Exception("Headers not found: %s" % hdrs)
+        hdrs = ['sn', 'ts.code', 'ts.name', 'ts.loc', 'xfmr.id']
+        self._ensure_headers_exist(hdrs)
         
         # iterate rows and extract asset in nested-form
         station, feeders = None, None
@@ -76,6 +92,7 @@ class Reader:
                 station = _({
                     'code': get(row, cols.ts_code),
                     'name': get(row, cols.ts_name),
+                    'location': get(row, cols.ts_loc),
                     'type': 'transmission',
                     'transformers': []
                 })
@@ -93,7 +110,7 @@ class Reader:
             feeders.append(_({
                 'code': get(row, cols.fdr_code),
                 'voltage': get(row, cols.fdr_volt),
-                'name': get(row, cols.fdr_name)
+                'name': get(row, cols.fdr_name).strip().title()
             }))
         
         # return very last station
@@ -102,12 +119,11 @@ class Reader:
     
     def _get_injection_assets(self, cols):
         # helper
-        get = lambda r, label: r[label] or ''
+        get = self._get_row_entry
         
         # find headers
-        hdrs = ['sn', 'source', 'is.name', 'is.type']
-        if not XlSheet.find_headers(self.ws, hdrs, 0):
-            raise Exception("Headers not found: %s" % hdrs)
+        hdrs = ['sn', 'source', 'is.name', 'is.loc', 'is.type']
+        self._ensure_headers_exist(hdrs)
         
         # iterate rows, extract data & push into collection
         station, feeders, count = None, None, 0
@@ -122,6 +138,7 @@ class Reader:
                 station = _({
                     'code': 'IS{:0>2}'.format(hex(count)[2:]).upper(),
                     'name': get(row, cols.is_name),
+                    'location': get(row, cols.is_loc),
                     'type': 'injection',
                     'source_feeder': get(row, cols.source),
                     'is_public': is_public,
@@ -141,12 +158,58 @@ class Reader:
                 feeders.append(_({
                     'code': get(row, cols.fdr_code),
                     'voltage': get(row, cols.fdr_volt),
-                    'name': get(row, cols.fdr_name)
+                    'name': get(row, cols.fdr_name).strip().title()
                 }))
         
         # return very last station
         if station:
             yield station
+    
+    def _get_distribution_assets(self, cols):
+        # helper
+        get = self._get_row_entry
+        
+        # find headers
+        hdrs = ['fdr.sn', 'fdr.volt', 'fdr.name', 'ss.sn']
+        self._ensure_headers_exist(hdrs)
+        
+        # iterate rows and extract asset in nested-form
+        source_feeder, skip_count = None, 0
+        for row in self.ws:
+            # new source_feeder encountered
+            if get(row, cols.fdr_name):
+                source_feeder = _({
+                    'volt': get(row, cols.fdr_volt),
+                    'name': get(row, cols.fdr_name).strip().title(),
+                })
+            
+            # ensure a source_feeder exist at this point otherwise
+            # ignore current line and proceed to the next line even 
+            # if it has substation details.
+            if not source_feeder:
+                skip_count += 1
+                continue
+            
+            ## from original list sub-station name needs to be normalized
+            ss_name = get(row, cols.ss_name).title().strip()
+            if ss_name.endswith(','):
+                ss_name = (ss_name[:-1]).strip()
+            
+            is_public = get(row, cols.ss_type).lower() == 'p'
+            station = _({
+                'code': 'Sx',
+                'name': ss_name,
+                'type': 'distribution',
+                'source_feeder': source_feeder.name,
+                'is_public': is_public,
+                'transformers': [_({
+                    'name': 'TR1',
+                    'capacity': get(row, cols.ss_cap),
+                    'upriser_count': 0
+                })]
+            })
+            yield station
+        print('# lines skipped: %s' % skip_count)
 
 
 class Loader:
@@ -192,14 +255,14 @@ class Loader:
         db.stations.insert(station)
         
         if feeders:
-            db.feeders.insert(feeders)
+            db.feeders.insert_many(feeders)
         
         msg_fmt = 'assets:: station: {0:<30}, transformers: {1:<3}, feeders: {2:<3}'
         args = (station.name, len(station.transformers), len(feeders))
         print(msg_fmt.format(*args))
     
     @staticmethod
-    def _load_network_stations_feeders(db, wb):
+    def _loadMV_network_stations_feeders(db, wb):
         args_list = (
             (TRANSMISSION, 'TStations+Fdrs'),
             (INJECTION,    'IStations+Fdrs')
@@ -211,15 +274,69 @@ class Loader:
                 Loader._save_as_stations_feeders(db, asset)
     
     @staticmethod
-    def load():
-        filename = os.path.join(DATA_DIR, 'kedco-ntwk-assets.xlsx')
-        wb = load_workbook(filename)
-        db = MongoClient().kedco
+    def _loadLV_network_stations(db, wb):
+        # extra 11kV feeder list from db
+        cursor = db.feeders\
+                   .find({'voltage':11, 'name': {'$ne': ''}})\
+                   .sort('name', pymongo.ASCENDING)
         
-        Loader._load_network_stations_feeders(db, wb)
+        feeders = {f['name']:f['code'] for f in cursor}
+        
+        # get dist. sub-stations from file
+        assets, not_found, xlfeeders = [], [], set()
+        ws = XlSheet(wb, 'DStations-11KV')
+        reader = Reader(ws, DISTRIBUTION)
+        for asset in reader.get_assets():
+            assets.append(asset)
+            
+            # change feeder name to feeder code
+            name = asset['source_feeder']
+            xlfeeders.add(name)
+            if name not in feeders:
+                not_found.append(name)
+            else:
+                asset['source_feeder'] = feeders[name]
+            
+            # save to db
+            db.stations.insert_one(asset)
+        print('assets:: stations: %s' % len(assets))
+
+
+def run(task):
+    filename = os.path.join(DATA_DIR, 'kedco-ntwk-assets.xlsx')
+    wb = load_workbook(filename)
+    db = pymongo.MongoClient().kedco
+    
+    task(db, wb)
+
+
+def clear_mongo_db(db, wb):
+    db.stations.delete_many({})
+    db.feeders.delete_many({})
+
+
+def collect_injss_sorted(db, wb):
+    cursor = db.stations\
+               .find({'type':'injection'})\
+               .sort([('is_public', pymongo.ASCENDING),
+                      ('location', pymongo.ASCENDING),
+                      ('name', pymongo.ASCENDING)])
+
+    with open(os.path.join(DATA_DIR, 'injss-sorted.txt'), 'w') as f:
+        for station in cursor:
+            f.write('{}, {}, {}\n'.format(
+                'p' if station['is_public'] else 'd',
+                station['location'],
+                station['name']
+            ))
+        f.flush()
+
 
 
 if __name__ == '__main__':
-    Loader.load()
-    pass
+    #run(clear_mongo_db)
+    #run(Loader._loadMV_network_stations_feeders)
+    #run(Loader._loadLV_network_stations)
+    run(collect_injss_sorted)
+    print('done!')
 
