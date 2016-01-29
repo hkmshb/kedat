92b9e1262b2c5edea1cd258034ebfed15b15a300
hkmshb <hkmshb@gmail.com>
2016-01-29 17:03:46 +0100

    Update settings to get token keys from a module not tracked by the dvcs

--- scripts/centrak/settings.py
+++ scripts/centrak/settings.py
@@ -3,6 +3,7 @@ Application wide settings.
 """
 import os
 import sys
+from secret import *
 
 
 # directory paths
@@ -12,10 +13,6 @@ sys.path.append(KEDAT_DIR)
 
 
 # API
-API_BASE_URL='http://survey.kedco.ng/api/v1/%s'
-
-API_TOKEN_SURVEY_TEST='4b84e4ee77128d8e1d7801fb9bbf3e1c3954ed1e'
-API_TOKEN_EAGLE_EYE='12a13a30fb3d3016826240840649d9b966abc4f6'
 API_DEFAULT_TOKEN=API_TOKEN_EAGLE_EYE
 
 AUTH_HEADER = {
